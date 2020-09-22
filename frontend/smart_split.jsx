132d853cedb454d24aebd558315e8d1a03d6209e
rachanavishwanath <rachana.gujrathi@gmail.com>
2020-09-22 06:40:08 -0700

    Making even

--- frontend/smart_split.jsx
+++ frontend/smart_split.jsx
@@ -13,7 +13,20 @@ document.addEventListener('DOMContentLoaded',() => {
     window.login = login;
     window.logout = logout;
     window.signup = signup;
-    const store = configureStore();
+    let store;
+    debugger
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: { 
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { id: window.currentUser.id}
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
     window.store = store;
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={store}/>, root);
