7c56e2745bd8d7680075d0fd8ae5411c07144cc6
rachanavishwanath <rachana.gujrathi@gmail.com>
2020-10-31 13:11:55 -0700

    Trying to deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,8 @@ Rails.application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => "https://smartsplit2.herokuapp.com/#/"
-    :user_name            => ENV['GMAIL_USERNAME'],
-    :password             => ENV['GMAIL_PASSWORD'],
+    :user_name            => ENV["GMAIL_USERNAME"],
+    :password             => ENV["GMAIL_PASSWORD"],
     :authentication       => "plain",
     :enable_starttls_auto => true,   
   }
