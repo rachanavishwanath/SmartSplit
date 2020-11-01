e28f56bdd010dde1f0ffac327a48ed02080f5c1e
rachanavishwanath <rachana.gujrathi@gmail.com>
2020-11-01 01:43:33 -0800

    Changed password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => "mydomain.com",
-    :user_name            => "smartsplitinvitations@gmail.com",
-    :password             => "SmartSplit!@#456",
+    :user_name            => ENV["GMAIL_USERNAME"],
+    :password             => ENV["GMAIL_PASSWORD"],
     :authentication       => 'plain',
     :enable_starttls_auto => true  
   }
